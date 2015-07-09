bc07f92187647ff8ea14a072b345601a59c5e65a
Gordon Fontenot <gordon@thoughtbot.com>
2015-07-09 15:04:42 -0400

    Initial Commit

---
+++ Source/Resources/Curry.h
@@ -0,0 +1,19 @@
+//
+//  Curry.h
+//  Curry
+//
+//  Created by Gordon Fontenot on 7/9/15.
+//  Copyright (c) 2015 thoughtbot. All rights reserved.
+//
+
+#import <Foundation/Foundation>
+
+//! Project version number for Curry.
+FOUNDATION_EXPORT double CurryVersionNumber;
+
+//! Project version string for Curry.
+FOUNDATION_EXPORT const unsigned char CurryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Curry/PublicHeader.h>
+
+
