17caca229c24b4dac341ccaa3003d04d4b7d4588
Gordon Fontenot <gordon@thoughtbot.com>
2015-07-09 15:17:02 -0400

    Fix the build
    I don't know how these got messed up in the first place.

--- Source/Resources/Curry.h
+++ Source/Resources/Curry.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 thoughtbot. All rights reserved.
 //
 
-#import <Foundation/Foundation>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Curry.
 FOUNDATION_EXPORT double CurryVersionNumber;
