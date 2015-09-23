de5ea2711e9fa151ac534c540bcf5a9e738c25b5
Tony DiPasquale <tony@thoughtbot.com>
2015-09-23 14:33:32 -0700

    Rewrite `curry` function generator
    This generator has been updated to Swift 2.0 and also has been rewritten
    to generate endless `curry` functions by concatenating the alphabet as
    it goes, ie ...Y, Z, AA, AB, etc.
    
    The biggest change is that it was rewritten to use the other curry
    syntax, `(a: A)(b: B)` instead of `A -> B`. This is easier on the
    compiler and we can far extend the limit. It also compiles instantly.
    
    The limit of generic parameters was found to be 64, which I think is
    interestingly a power of 2. I wonder if that number is either hard
    coded in Swift or if Swift internally uses a small amount of bits for
    this. ANYWAYS, I think if you need more than a 64 parameter `curry`
    function then you're probably doing it wrong.

--- generator.swift
+++ generator.swift
@@ -2,25 +2,26 @@
 
 import Foundation
 
-let generics = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "AA"]
+let generics = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-// Set the threshold of arguments number after which the compiler cannot keep up
-let threshold: Int = (Process.arguments.count >= 2) ? Int(Process.arguments[1])! : 16
-
-let compileTimeIssueComment = "Compile time become exponential past this point"
-
-let outputPath = "Source/Curry.swift"
+let genericsGenerator: Int -> String = { x in
+  let max = generics.count
+  switch x {
+  case _ where x < max: return generics[x % max]
+  default: return generics[x / max - 1] + generics[x % max]
+  }
+}
 
 let curryGenerator: (arguments: Int) -> String = { arguments in
-  let types = ", ".join(generics[0..<arguments])
-  let functionTypes = ", ".join(generics[0..<arguments - 1])
-  let returnType = " -> ".join(generics[0..<arguments])
-  let ins = " in { ".join(generics[0..<arguments - 1]).lowercaseString
-  let functionArguments = functionTypes.lowercaseString
-  let trailingBrackets = " ".join((0..<arguments - 2).map { Int -> String in "}" })
+  let totalGenerics = (0..<arguments).map(genericsGenerator)
+  let types = totalGenerics[0..<arguments].joinWithSeparator(", ")
+  let functionTypes = totalGenerics[0..<arguments - 1].joinWithSeparator(", ")
+  let parameters = totalGenerics[0..<arguments - 1].map { "(_ `\($0.lowercaseString)`: \($0))" }.joinWithSeparator("")
+  let returnType = totalGenerics[arguments - 1]
+  let functionArguments = totalGenerics[0..<arguments - 1].map { "`\($0.lowercaseString)`" }.joinWithSeparator(", ")
 
-  var curry = "public func curry<\(types)>(function: (\(functionTypes)) -> \(generics[arguments - 1])) -> \(returnType) {"
-  curry += "\n    return { \(ins) in function(\(functionArguments)) \(trailingBrackets) }"
+  var curry = "public func curry<\(types)>(function: (\(functionTypes)) -> \(returnType))\(parameters) -> \(returnType) {"
+  curry += "\n    return function(\(functionArguments))"
   curry += "\n}"
 
   return curry
@@ -28,28 +29,20 @@ let curryGenerator: (arguments: Int) -> String = { arguments in
 
 print("Generating 💬")
 
-// We start with 3 because that correspond to (A, B) -> C that then becomes A -> B -> C
-// If we were using 2 it would be (A) -> B that then becomes A -> B, which is the same
-let start = 3
-let curries = (start...generics.count).map { curryGenerator(arguments: $0) }
-let usableCurries = curries[0...threshold - start]
-let unusableCurries = curries[threshold - start + 1..<curries.count]
+let start = 2
+let knownLimitOfGenericParameters = 64
+let curries = (start..<knownLimitOfGenericParameters).map { curryGenerator(arguments: $0) }
 
-let newLine = "\n\n"
-let comment = "//"
-var output = newLine.join(usableCurries)
-output += newLine + comment + " " + compileTimeIssueComment + newLine
-// Here we split each unusable curry method into its lines, add a comment at the start of each, then reassemble into a single string
-output += newLine.join(unusableCurries.map { "\n".join(split($0.characters) { $0 == "\n" }.map { String.init($0) }.map { comment + $0 }) })
-output += "\n"
+let output = curries.joinWithSeparator("\n\n") + "\n"
 
-let currentPath = NSFileManager.defaultManager().currentDirectoryPath
-let currySwiftPath = currentPath.stringByAppendingPathComponent(outputPath)
+let outputPath = "Source/Curry.swift"
+let currentPath = NSURL(fileURLWithPath: NSFileManager.defaultManager().currentDirectoryPath)
+let currySwiftPath = currentPath.URLByAppendingPathComponent(outputPath)
 
 do {
-  try output.writeToFile(currySwiftPath, atomically: true, encoding: NSUTF8StringEncoding)
-} catch _ {
-  print("An error occurred while saving the generated functions to \"\(output)\"")
+  try output.writeToURL(currySwiftPath, atomically: true, encoding: NSUTF8StringEncoding)
+} catch let e as NSError {
+  print("An error occurred while saving the generated functions. Error: \(e)")
 }
 
 print("Done, curry functions files written at \(outputPath) 👍")
