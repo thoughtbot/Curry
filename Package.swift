c3e4e1a414fd7c0c119b4999a5495ad3f1d7100c
Pierre TACCHI <pyroh@icloud.com>
2019-01-30 18:36:36 +0100

    Update Package.swift
    Brings Swift 5.0 compatibility.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,10 @@
+// swift-tools-version:4.2
+
 import PackageDescription
 
 let package = Package(
-  name: "Curry"
+  name: "Curry",
+  products: [.library(name: "Curry", targets: ["Curry"])],
+  dependencies: [],
+  targets: [.target(name: "Curry", dependencies: [], path: ".")]
 )
